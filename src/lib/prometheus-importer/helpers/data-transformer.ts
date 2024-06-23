4818b55abe66e1eef84c9bb395a29400cfd69014
Shivani-G <shivaniarchana@gmail.com>
2024-06-23 21:18:13 +0900

    add data-transformer helper

---
+++ src/lib/prometheus-importer/helpers/data-transformer.ts
@@ -0,0 +1,44 @@
+import {DataTransformer} from '../interfaces';
+
+export const parseAndEnrichDataTransformer = (): DataTransformer => {
+  const parseMetrics = async (
+    rawResponse: Record<string, any>,
+    metricLabels: Array<string>,
+    metricName: string,
+    defaultLabels: Record<string, any>
+  ) => {
+    const x = await rawResponse;
+    const output: Array<Record<string, any>> = [];
+    for (const elm of x.data.result) {
+      const metricLabelKeyValuePairs = getMetricLabels(
+        metricLabels,
+        elm.metric
+      );
+      for (const [timestamp, value] of elm.values) {
+        output.push({
+          timestamp: timestamp,
+          ...metricLabelKeyValuePairs,
+          ...defaultLabels,
+          [metricName]: value,
+        });
+      }
+    }
+    return output;
+  };
+
+  const getMetricLabels = async (
+    metricLabels: Array<string>,
+    rawMetricObject: Record<string, any>
+  ) => {
+    const x = await rawMetricObject;
+    const output: Record<string, any> = {};
+    for (const metricLabel of metricLabels) {
+      output[metricLabel] = x[metricLabel];
+    }
+    return output;
+  };
+
+  return {
+    parseMetrics,
+  };
+};
